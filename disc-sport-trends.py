71f8a65975480c8e39ef04181f0cbce04d78b48a
Johannes Schöck <38401818+JSchoeck@users.noreply.github.com>
2021-12-23 08:21:28 +0100

    Update disc-sport-trends.py

--- disc-sport-trends.py
+++ disc-sport-trends.py
@@ -16,6 +16,8 @@ import streamlit as st
 * NLTK
 * pytrends
 
+---
+
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
